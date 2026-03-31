6c34edfb32cf164f48ff3c01b23af503f096b8c9
Choi Seungwook <agion427@gmail.com>
2026-03-31 10:20:22 +0900

    feat : yaml 파일 변수 주입 기능 추가

--- terraform/cloudbuild.tf
+++ terraform/cloudbuild.tf
@@ -1,12 +1,21 @@
 resource "google_cloudbuild_trigger" "backend_push_trigger" {
-
   name     = "backend-trigger"
-  location = "asia-northeast3"
+  location = var.region 
 
-  service_account = "projects/knu-2026-agion427/serviceAccounts/675009148577-compute@developer.gserviceaccount.com"
+  service_account = "projects/${var.project_id}/serviceAccounts/675009148577-compute@developer.gserviceaccount.com"
+
+  substitutions = {
+    _REGION        = var.region
+    _ZONE          = var.zone
+    _REPO_NAME     = google_artifact_registry_repository.main.repository_id
+    _IMAGE_NAME    = "backend"
+    _INSTANCE_NAME = google_compute_instance.backend.name
+    _ENV_FILE      = "/home/ubuntu/configs/.env"
+    _PORT          = "8080"
+  }
 
   repository_event_config {
-    repository = "projects/knu-2026-agion427/locations/asia-northeast3/connections/Backend/repositories/FitTruck-Isajjim-Backend"
+    repository = "projects/${var.project_id}/locations/${var.region}/connections/Backend/repositories/FitTruck-Isajjim-Backend"
     
     push {
       branch = "^dev$"
